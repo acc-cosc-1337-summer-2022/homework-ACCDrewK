ecc2e7360ea815c03b98d903384f3f1296aa4703
ACCDrewK <andrew.kane@g.austincc.edu>
2022-06-25 21:39:20 -0500

    Homework 4

--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,5 +1,13 @@
-//add include statements
-
+#ifndef DNA_H
+#define DNA_H
+// add include statements
+#include <string>
 /*
 Write prototype for function(s)
 */
+double get_gc_content(const std::string &);
+std::string get_reverse_string(std::string);
+std::string get_dna_complement(std::string);
+int display_menu();
+std::string get_dna_string();
+#endif
